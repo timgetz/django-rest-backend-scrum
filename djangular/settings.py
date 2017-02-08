db42be55382b85a0281c4a4cefc8806e38ba5e43
Tim Getz <tgetz@arcweb.co>
2017-02-07 20:42:38 -0500

    Disabled CORS security

--- djangular/settings.py
+++ djangular/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'scrumboard'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS Settings
+CORS_ORIGIN_ALLOW_ALL = True
